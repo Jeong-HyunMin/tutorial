2381b310e44a19fa80143a05ea0f17983ed10cf7
정현민 <gusals0928@naver.com>
2018-05-23 20:16:27 +0900

    콤보박스 추가함

--- FileManager/src/FileManager.java
+++ FileManager/src/FileManager.java
@@ -6,10 +6,12 @@ import javax.swing.*;
 
 public class FileManager extends JFrame{
 
+	String language [] = {"한국어", "English"};
 	JPanel p1 = new JPanel();
 	JPanel p2 = new JPanel();
 	JPanel p3 = new JPanel();
 	JPanel p4 = new JPanel();
+	JComboBox<String> cb1 = new JComboBox<String>(language);
 	
 	public FileManager() {
 		super ("FileManager");
@@ -18,7 +20,9 @@ public class FileManager extends JFrame{
 		add (p2, BorderLayout.WEST);
 		add (p3, BorderLayout.SOUTH);
 		add (p4, BorderLayout.CENTER);
+		p3.add (cb1);
 		setSize (600, 400);				//1366*768 해상도 최적
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible (true);
 		
 	}
